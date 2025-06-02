0cd1465e3cd1e1f64ab94f7668c6d3aed5c3df97
martis <martynas.grumadas@gmail.com>
2025-06-02 21:23:29 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TodoApp from './components/List/List';
+import ToggleDetails from './components/useCounter/useCounter';
+
+function App() {
+  return (
+    <>
+      <ToggleDetails />
+      <TodoApp />
+    </>
+  );
+}
+
+export default App;
