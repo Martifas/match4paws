84959d8d9365844afcf69e323ecf36a125ae3c3a
mgrumadas <Martynas.Grumadas@craneauthentication.com>
2025-06-03 12:36:21 +0300

    migrate to next.js

---
+++ build/server/chunks/ssr/src_app_[[___slug]]_client_tsx_6945a8f5._.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+"[project]/src/app/[[...slug]]/client.tsx [app-ssr] (ecmascript)": ((__turbopack_context__) => {
+"use strict";
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.s({
+    "ClientOnly": (()=>ClientOnly)
+});
+"use client";
+function ClientOnly() {
+    return null;
+}
+}}),
+
+};
+
+//# sourceMappingURL=src_app_%5B%5B___slug%5D%5D_client_tsx_6945a8f5._.js.map
