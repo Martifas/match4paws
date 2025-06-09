9005ced1fd37087bf61b912c38bc5d3bec390f67
mgrumadas <Martynas.Grumadas@craneauthentication.com>
2025-06-09 15:29:07 +0300

    adding container for proper responsiviness

--- build/static/chunks/src_app_[[___slug]]_page_tsx_8d0ece02._.js
+++ build/static/chunks/src_app_[[___slug]]_page_tsx_8d0ece02._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_a5cdca5b._.js",
-  "static/chunks/src_3da14570._.js",
+  "static/chunks/node_modules_9a720578._.js",
+  "static/chunks/src_f1c84be4._.js",
   "static/chunks/src_components_Onboarding_scss_db5da33f.css"
 ],
     source: "dynamic"
