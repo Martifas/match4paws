66022597b7000bb9f6bcdb28a71b4d8e55be2278
mgrumadas <Martynas.Grumadas@craneauthentication.com>
2025-06-09 13:50:24 +0300

    stepper added

---
+++ build/static/chunks/src_app_[[___slug]]_page_tsx_8d0ece02._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a5cdca5b._.js",
+  "static/chunks/src_3da14570._.js",
+  "static/chunks/src_components_Onboarding_scss_db5da33f.css"
+],
+    source: "dynamic"
+});
--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -1,14 +1,34 @@
-import PawIcon from "@/assets/PawIcon";
+"use client";
+
+import { useState } from "react";
+import Loading from "./Loading";
+import DogPic from "../assets/dog-photo.png";
+import Image from "next/image";
+import "./Onboarding.scss";
+import ProgressBar from "./ProgressBar";
 
 function Onboarding() {
+  const [loading, setLoading] = useState(false);
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <div className="flex flex-col items-center h-screen bg-[#ed9426]">
-      <div className="flex-1 flex flex-col items-center justify-center gap-4">
-        <PawIcon />
-        <h1 className="text-white text-3xl font-bold">Adoptify</h1>
+    <div className="flex flex-col items-center h-screen bg-white">
+      <div className="curved-section">
+        <Image src={DogPic} alt="Dog Pic" />
       </div>
-      <div className="pb-20">
-        <h2 className="text-white text-2xl">Loading</h2>
+      <div className="flex flex-col bg-white text-wrap text-center">
+        <h2 className="bg-white font-bold text-3xl">
+          Match4Paws - Where Furry Tales Begin
+        </h2>
+        <p>
+          Embark on a heartwarming journey to find your perfect companion.
+          Swipe, match, and open your heart to a new furry friend.
+        </p>
+        <div>
+          <ProgressBar />
+        </div>
       </div>
     </div>
   );
