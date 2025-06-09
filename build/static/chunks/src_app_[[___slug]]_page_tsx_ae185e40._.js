28db023868f0773b4fc6461c12a636bd7958b243
mgrumadas <Martynas.Grumadas@craneauthentication.com>
2025-06-09 12:17:51 +0300

    onboarding screen styles &tailwind added

--- build/static/chunks/src_app_[[___slug]]_page_tsx_ae185e40._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_Onboarding_scss_db5da33f.css"
-],
-    source: "dynamic"
-});
