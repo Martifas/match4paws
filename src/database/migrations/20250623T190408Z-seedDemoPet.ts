768686ada48835d8cd877bf83fd9a4828b17e7a2
mgrumadas <Martynas.Grumadas@craneauthentication.com>
2025-06-25 14:56:20 +0300

    database seed changed

--- src/database/migrations/20250623T190408Z-seedDemoPet.ts
+++ src/database/migrations/20250623T190408Z-seedDemoPet.ts
@@ -3,22 +3,49 @@ import type { Kysely } from "kysely";
 import { randomUUID } from "crypto";
 
 export async function up(db: Kysely<unknown>) {
-  const ownerId = "0fd48ffa-35e9-4a6d-9cf5-34e5daf7fc7b";
-  const petId = randomUUID();
+  const ownerId = "6714e5bb-d143-47ca-a193-93460d120fa6";
+
+  const merfisId = randomUUID();
+  const vulkanasId = randomUUID();
+  const likaId = randomUUID();
 
   await (db as Kysely<any>)
     .insertInto("pets")
-    .values({
-      id: petId,
-      owner_id: ownerId,
-      type: "dog",
-      gender: "male",
-      size: "small",
-      age_group: "young",
-      name: "Merfis",
-      breed: "Mix",
-      description: "Friendly and loves fetch.",
-    })
+    .values([
+      {
+        id: merfisId,
+        owner_id: ownerId,
+        type: "dog",
+        gender: "male",
+        size: "small",
+        age_group: "young",
+        name: "Merfis",
+        breed: "Mix",
+        description: "Friendly and loves fetch.",
+      },
+      {
+        id: vulkanasId,
+        owner_id: ownerId,
+        type: "dog",
+        gender: "male",
+        size: "small",
+        age_group: "adult",
+        name: "Vulkanas",
+        breed: "Mix",
+        description: "Friendly and loves fetch.",
+      },
+      {
+        id: likaId,
+        owner_id: ownerId,
+        type: "cat",
+        gender: "female",
+        size: "small",
+        age_group: "adult",
+        name: "Lika",
+        breed: "Mix",
+        description: "Love to play with ball.",
+      },
+    ])
     .execute();
 
   await (db as Kysely<any>)
@@ -26,14 +53,38 @@ export async function up(db: Kysely<unknown>) {
     .values([
       {
         id: randomUUID(),
-        pet_id: petId,
+        pet_id: merfisId,
         url: "http://www.beglobis.com/page/upload/modproduct_6821dede02ad7_b1.jpg",
         order_idx: 0,
       },
       {
         id: randomUUID(),
-        pet_id: petId,
+        pet_id: merfisId,
         url: "http://www.beglobis.com/page/upload/modproduct_6821df148382d_b1.jpg",
+        order_idx: 1,
+      },
+      {
+        id: randomUUID(),
+        pet_id: vulkanasId,
+        url: "http://www.beglobis.com/page/upload/modproduct_680eb7e372443_b1.jpg",
+        order_idx: 0,
+      },
+      {
+        id: randomUUID(),
+        pet_id: vulkanasId,
+        url: "http://www.beglobis.com/page/upload/modproduct_680eb81a564fd_b1.jpg",
+        order_idx: 1,
+      },
+      {
+        id: randomUUID(),
+        pet_id: likaId,
+        url: "http://www.beglobis.com/page/upload/modproduct_5c8abfd6bcb1a_b1.jpg",
+        order_idx: 0,
+      },
+      {
+        id: randomUUID(),
+        pet_id: likaId,
+        url: "http://www.beglobis.com/page/upload/modproduct_5c8ac1b5d6f6c_b1.jpg",
         order_idx: 1,
       },
     ])
@@ -43,20 +94,19 @@ export async function up(db: Kysely<unknown>) {
 export async function down(db: Kysely<unknown>) {
   await (db as Kysely<any>)
     .deleteFrom("pet_images")
-    .where(
-      "url",
-      "=",
-      "http://www.beglobis.com/page/upload/modproduct_6821df148382d_b1.jpg"
-    )
-    .where(
-      "url",
-      "=",
-      "http://www.beglobis.com/page/upload/modproduct_6821dede02ad7_b1.jpg"
-    )
+    .where("url", "in", [
+      "http://www.beglobis.com/page/upload/modproduct_6821dede02ad7_b1.jpg",
+      "http://www.beglobis.com/page/upload/modproduct_6821df148382d_b1.jpg",
+      "http://www.beglobis.com/page/upload/modproduct_680eb7e372443_b1.jpg",
+      "http://www.beglobis.com/page/upload/modproduct_680eb81a564fd_b1.jpg",
+      "http://www.beglobis.com/page/upload/modproduct_5c8ac1b5d6f6c_b1.jpg",
+      "http://www.beglobis.com/page/upload/modproduct_5c8abfd6bcb1a_b1.jpg",
+    ])
     .execute();
 
+  // Then remove the pets
   await (db as Kysely<any>)
     .deleteFrom("pets")
-    .where("name", "=", "Buddy")
+    .where("name", "in", ["Merfis", "Vulkanas", "Lika"])
     .execute();
 }
